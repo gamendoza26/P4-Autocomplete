f41f1f0b727e8ebe306e6d852ebfcd15bb78c601
Gabriel Mendoza <gabriel.mendoza@duke.edu>
2023-03-27 15:38:57 -0400

    Tried to fix code errors

--- src/HashListAutocomplete.java
+++ src/HashListAutocomplete.java
@@ -19,7 +19,7 @@ public class HashListAutocomplete implements Autocompletor {
 
     @Override
     public List<Term> topMatches(String prefix, int k) {
-        if (prefix.length() > MAX_PREFIX) {
+        if (prefix.length() >= MAX_PREFIX) {
             prefix = prefix.substring(0,MAX_PREFIX);
         }
         if (myMap.containsKey(prefix)) {
